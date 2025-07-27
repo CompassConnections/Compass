6b0e343412c38fecd6de229717ecb32a6fa68917
MartinBraquet <martin.braquet@gmail.com>
2025-07-27 22:22:46 +0200

    Redirect url

--- app/Header.tsx
+++ app/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
                   <div>{session.user?.email}</div>
                 </div>
                 <button
-                  onClick={() => signOut()}
+                  onClick={() => signOut({callbackUrl: "/"})}
                   className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
                 >
                   Sign Out

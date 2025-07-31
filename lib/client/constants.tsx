1256badc6569810f473a2c7d421b2c30fafd5cc9
MartinBraquet <martin.braquet@gmail.com>
2025-07-31 16:28:02 +0200

    Fix use client

--- lib/client/constants.tsx
+++ lib/client/constants.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 export const supportEmail = 'mysharktrash@gmail.com (official email to be created)';
 
 export const pStyle = "mt-1 text-gray-800 dark:text-white whitespace-pre-line";

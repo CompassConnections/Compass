e14a4889344bc8834df955887d9e921ca775b4d5
MartinBraquet <martin.braquet@gmail.com>
2025-09-11 22:35:54 +0200

    Revert "Failed attempt to use react icons in emails"
    This reverts commit e82a8d9bc38e39d452678e6da696db425c2325d0.

--- backend/email/emails/utils.tsx
+++ backend/email/emails/utils.tsx
@@ -1,21 +1,5 @@
 import {Link, Row, Section, Text} from "@react-email/components";
-import {
-  TbBrandBluesky,
-  TbBrandDiscord,
-  TbBrandFacebook,
-  TbBrandGithub,
-  TbBrandInstagram,
-  TbBrandLinkedin,
-  TbBrandMastodon,
-  TbBrandPatreon,
-  TbBrandPaypal,
-  TbBrandSpotify,
-  TbBrandX
-} from "react-icons/tb";
-
-import {LinkIcon} from '@heroicons/react/solid'
-import {Site} from 'common/src/socials'
-import {LuBookmark} from 'react-icons/lu'
+import {SocialIcon} from "web/components/user/social";
 
 interface Props {
   email?: string
@@ -35,7 +19,7 @@ export const Footer = ({
       <Row>
         <div></div>
         <Link href="https://github.com/CompassMeet/Compass">
-          <SocialIcon site={'github'} size={36} color={'black'}/>
+          <SocialIcon site={'github'} size={36} color={'black'} />
         </Link>
         <Link href="https://discord.gg/8Vd7jzqjun">
           <SocialIcon site={'discord'} size={36} color={'black'}/>
@@ -56,37 +40,6 @@ export const Footer = ({
       </Text>
     </Row>
   </Section>
-}
-
-
-export const PLATFORM_ICONS: {
-  [key in Site]: (props: { className?: string }) => any
-} = {
-  site: LinkIcon,
-  x: TbBrandX,
-  discord: TbBrandDiscord,
-  bluesky: TbBrandBluesky,
-  mastodon: TbBrandMastodon,
-  substack: LuBookmark,
-  instagram: TbBrandInstagram,
-  github: TbBrandGithub,
-  linkedin: TbBrandLinkedin,
-  facebook: TbBrandFacebook,
-  spotify: TbBrandSpotify,
-  patreon: TbBrandPatreon,
-  paypal: TbBrandPaypal,
-}
-
-export const SocialIcon = (props: {
-  site: string;
-  className?: string;
-  size?: number;
-  color?: string;
-}): React.ReactElement | null => {
-  const {site, ...rest} = props
-  const Icon = PLATFORM_ICONS[site as Site] || PLATFORM_ICONS.site
-
-  return <Icon {...rest} />
 }
 
 export const footer = {

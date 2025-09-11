8e11657bd28cf421aad22bcd40791859a235ba50
MartinBraquet <martin.braquet@gmail.com>
2025-09-12 01:26:49 +0200

    Update install

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ cd "$(dirname "$0")"
 
 rm -rf node_modules web/node_modules backend/api/node_modules backend/email/node_modules command/node_modules backend/shared/node_modules
 
-yarn install --frozen-lockfile
+yarn install
 

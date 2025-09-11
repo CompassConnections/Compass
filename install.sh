f2b2c02cd6673e39a414558394893f75dd48a38d
MartinBraquet <martin.braquet@gmail.com>
2025-09-11 22:56:29 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+rm -rf node_modules web/node_modules backend/api/node_modules backend/email/node_modules command/node_modules backend/shared/node_modules
+
+yarn install
+

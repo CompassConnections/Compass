c6684af521d7dfce03cbd4c85a2c942b212613c8
MartinBraquet <martin.braquet@gmail.com>
2025-09-13 23:17:23 +0200

    Improve colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -313,6 +313,15 @@ module.exports = {
           25: 'rgb(var(--color-canvas-25) / <alpha-value>)',
           50: 'rgb(var(--color-canvas-50) / <alpha-value>)',
           100: 'rgb(var(--color-canvas-100) / <alpha-value>)',
+          200: 'rgb(var(--color-canvas-200) / <alpha-value>)',
+          300: 'rgb(var(--color-canvas-300) / <alpha-value>)',
+          400: 'rgb(var(--color-canvas-400) / <alpha-value>)',
+          500: 'rgb(var(--color-canvas-500) / <alpha-value>)',
+          600: 'rgb(var(--color-canvas-600) / <alpha-value>)',
+          700: 'rgb(var(--color-canvas-700) / <alpha-value>)',
+          800: 'rgb(var(--color-canvas-800) / <alpha-value>)',
+          900: 'rgb(var(--color-canvas-900) / <alpha-value>)',
+          950: 'rgb(var(--color-canvas-950) / <alpha-value>)',
         },
 
         primary: {

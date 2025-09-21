90522cb88b6ab437f7ce021f5e0197f3862b68b8
MartinBraquet <martin.braquet@gmail.com>
2025-09-22 00:05:15 +0200

    Comment log

--- backend/shared/src/firebase-utils.ts
+++ backend/shared/src/firebase-utils.ts
@@ -3,7 +3,7 @@ import {ENV_CONFIG} from "common/envs/constants";
 
 export const getServiceAccountCredentials = () => {
   let keyPath = ENV_CONFIG.googleApplicationCredentials
-  console.log('Using GOOGLE_APPLICATION_CREDENTIALS:', keyPath)
+  // console.log('Using GOOGLE_APPLICATION_CREDENTIALS:', keyPath)
   if (!keyPath) {
     // throw new Error(
     //   `Please set the GOOGLE_APPLICATION_CREDENTIALS environment variable to contain the path to your key file.`

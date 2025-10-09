450f07f505339eea4cab28fb658a69166953a91b
MartinBraquet <martin.braquet@gmail.com>
2025-10-09 11:27:24 +0200

    Add private backup

--- backups/firebase/storage/backup.sh
+++ backups/firebase/storage/backup.sh
@@ -3,14 +3,14 @@
 set -e
 cd "$(dirname "$0")"
 
-PROJECT=compass-130ba
-
 TIMESTAMP=$(date +"%F_%H-%M-%S")
 
 DESTINATION=./data/$TIMESTAMP
 
 mkdir -p $DESTINATION
-gsutil -m cp -r gs://$PROJECT.firebasestorage.app $DESTINATION
+
+gsutil -m cp -r gs://compass-130ba.firebasestorage.app $DESTINATION
+gsutil -m cp -r gs://compass-130ba-private $DESTINATION
 
 echo Backup of Firebase Storage done
 

44666aec03cb193eea6684985b7bd105345c6e2a
MartinBraquet <martin.braquet@gmail.com>
2025-10-10 18:33:11 +0200

    Update post install

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -9,6 +9,9 @@ if [ ! -f .env ]; then
   echo ".env file created from .env.example"
 fi
 
+source .env.example
 source .env
+
+echo $GOOGLE_CREDENTIALS_ENC_PWD
 
 openssl enc -d -aes-256-cbc -pbkdf2 -iter 100000 -in secrets/googleApplicationCredentials-dev.json.enc -out backend/shared/src/googleApplicationCredentials-dev.json -pass pass:$GOOGLE_CREDENTIALS_ENC_PWD

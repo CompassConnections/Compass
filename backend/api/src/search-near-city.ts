6c6c1cc90ad280871b8d6d4fcf56af4bb612eb92
Martin Braquet <martin.braquet@gmail.com>
2025-10-12 15:58:52 +0200

    Upgrade geodb plan to increase radius and page limit  (#14)
    * Upgrade geodb plan to increase radius and page limit
    
    * Speed location debounce

--- backend/api/src/search-near-city.ts
+++ backend/api/src/search-near-city.ts
@@ -2,8 +2,7 @@ import {APIHandler} from './helpers/endpoint'
 import {geodbFetch} from "common/geodb";
 
 const searchNearCityMain = async (cityId: string, radius: number) => {
-  // Limit to 10 cities for now for free plan, was 100 before (may need to buy plan)
-  const endpoint = `/cities/${cityId}/nearbyCities?radius=${radius}&offset=0&sort=-population&limit=10`
+  const endpoint = `/cities/${cityId}/nearbyCities?radius=${radius}&offset=0&sort=-population&limit=100`
   return await geodbFetch(endpoint)
 }
 

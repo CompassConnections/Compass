47cef359ca3bce0933dcb8ba1ebf62e06db803e3
MartinBraquet <martin.braquet@gmail.com>
2025-10-22 03:57:57 +0200

    Put back icons

--- web/public/service-worker.js
+++ web/public/service-worker.js
@@ -44,8 +44,8 @@ self.addEventListener('push', event => {
     const title = data.title || 'Notification';
     const options = {
         body: data.body || 'You have a new message',
-        // icon: '/icons/icon-192x192.png',
-        // badge: '/icons/icon-192x192.png',
+        icon: '/icons/icon-192x192.png',
+        badge: '/icons/icon-192x192.png',
         data: data.url || '/'
     };
     event.waitUntil(self.registration.showNotification(title, options));

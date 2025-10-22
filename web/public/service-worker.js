a29b4a3a8ebdfa8d2bcf76ecd02877d5e774c04b
MartinBraquet <martin.braquet@gmail.com>
2025-10-22 03:40:15 +0200

    Remove badge

--- web/public/service-worker.js
+++ web/public/service-worker.js
@@ -43,7 +43,7 @@ self.addEventListener('push', event => {
     const options = {
         body: data.body || 'You have a new message',
         icon: '/icons/icon-192x192.png',
-        badge: '/icons/icon-192x192.png',
+        // badge: '/icons/icon-192x192.png',
         data: data.url || '/'
     };
     event.waitUntil(self.registration.showNotification(title, options));

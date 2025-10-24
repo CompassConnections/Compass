d0881b76e034ee92226b47a091f6d82d9f632691
MartinBraquet <martin.braquet@gmail.com>
2025-10-24 23:02:28 +0200

    Update migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -8,8 +8,8 @@ source .env
 export PGPASSWORD=$SUPABASE_DB_PASSWORD
 
 psql \
-  -h db.ltzepxnhhnrnvovqblfr.supabase.co \
+  -h db.zbspxezubpzxmuxciurg.supabase.co \
   -p 5432 \
   -d postgres \
   -U postgres \
-  -f backend/supabase/migration.sql
+  -f "$1" \

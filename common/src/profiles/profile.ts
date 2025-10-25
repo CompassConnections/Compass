d511e4a75ca9d25623351af21cdb49e5df0ce7c4
MartinBraquet <martin.braquet@gmail.com>
2025-10-25 23:00:46 +0200

    Comment log

--- common/src/profiles/profile.ts
+++ common/src/profiles/profile.ts
@@ -4,7 +4,7 @@ import { User } from 'common/user'
 export type ProfileRow = Row<'profiles'>
 export type Profile = ProfileRow & { user: User }
 export const getProfileRow = async (userId: string, db: SupabaseClient) => {
-  console.debug('getProfileRow', userId)
+  // console.debug('getProfileRow', userId)
   const res = await run(db.from('profiles').select('*').eq('user_id', userId))
   return res.data[0]
 }

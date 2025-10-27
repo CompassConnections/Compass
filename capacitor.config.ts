67b45f3e5c3478740af68c6d94312fe4b1d57a9f
MartinBraquet <martin.braquet@gmail.com>
2025-10-27 11:49:39 +0100

    Add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.compass.app',
+  appName: 'Compass',
+  webDir: 'web/.next'
+};
+
+export default config;

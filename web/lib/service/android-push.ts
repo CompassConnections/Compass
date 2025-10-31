27a72170b835c9a43a4fe9ae9fc1c8645ed1da07
MartinBraquet <martin.braquet@gmail.com>
2025-10-31 11:51:58 +0100

    Url redirect

--- web/lib/service/android-push.ts
+++ web/lib/service/android-push.ts
@@ -3,23 +3,29 @@ import {useEffect} from "react"
 import {api} from "web/lib/api"
 import {useUser} from "web/hooks/use-user"
 import {isAndroidWebView} from "web/lib/util/webview";
+import {useRouter} from "next/router";
 
 export default function AndroidPush() {
   const user = useUser() // authenticated user
   const isAndroid = isAndroidWebView()
+  const router = useRouter()
   useEffect(() => {
     if (!user?.id || !isAndroid) return
     console.log('AndroidPush', user)
 
     PushNotifications.requestPermissions().then(result => {
-      if (result.receive !== 'granted') return
+      if (result.receive !== 'granted') {
+        console.log('Push notifications not granted')
+        return
+      }
       PushNotifications.register()
+      console.log('Push registered')
     })
 
     PushNotifications.addListener('registration', async token => {
       console.log('Device token:', token.value)
       try {
-        const {data} = await api('save-subscription-mobile', { token: token.value })
+        const {data} = await api('save-subscription-mobile', {token: token.value})
         console.log('Mobile subscription saved:', data)
       } catch (err) {
         console.error('Failed saving android subscription', err)
@@ -28,6 +34,11 @@ export default function AndroidPush() {
 
     PushNotifications.addListener('pushNotificationReceived', notif => {
       console.log('Push received', notif)
+      const url = notif?.data?.url
+      if (url) {
+        router.push(url)
+        window.location.href = url
+      }
     })
   }, [user?.id, isAndroid])
 

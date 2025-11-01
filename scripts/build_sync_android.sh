71ec88d235d42cd1988113924942095cc924eee7
MartinBraquet <martin.braquet@gmail.com>
2025-11-01 22:05:28 +0100

    Add mobile icons

--- scripts/build_sync_android.sh
+++ scripts/build_sync_android.sh
@@ -12,4 +12,7 @@ source web/.env
 
 npx cap sync android
 
+# To generate icons
+# npx capacitor-assets generate --android
+
 # Then go to android studio, build, generate signed APK in android/app/release, adb install -r app-release.apk

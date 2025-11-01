ab82c66f83540d2745a1134ccb9f96b7c48f8100
MartinBraquet <martin.braquet@gmail.com>
2025-11-01 16:04:36 +0100

    Add bridge for social

--- android/app/src/main/java/com/compass/app/MainActivity.java
+++ android/app/src/main/java/com/compass/app/MainActivity.java
@@ -17,12 +17,15 @@ import androidx.core.content.ContextCompat;
 import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.BridgeWebViewClient;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginHandle;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
 
     // Declare this at class level
     private final ActivityResultLauncher<String> requestPermissionLauncher =
@@ -59,18 +62,18 @@ public class MainActivity extends BridgeActivity {
 
         String data = intent.getDataString();
         Log.i("CompassApp", "onNewIntent called with data: " + data);
-        if (data != null && data.startsWith("com.compassmeet://auth")) {
-            Log.i("CompassApp", "triggerWindowJSEvent oauthRedirect");
-            try {
-                String payload = new JSONObject().put("data", data).toString();
-                Log.i("CompassApp", "Payload: " + payload);
-                bridge.getWebView().post(() -> bridge.getWebView().evaluateJavascript("oauthRedirect(" + payload + ");", null));
-            } catch (JSONException e) {
-                Log.i("CompassApp", "Failed to encode JSON payload", e);
-            }
-        } else {
-            Log.i("CompassApp", "No relevant data");
-        }
+//        if (data != null && data.startsWith("com.compassmeet://auth")) {
+//            Log.i("CompassApp", "triggerWindowJSEvent oauthRedirect");
+//            try {
+//                String payload = new JSONObject().put("data", data).toString();
+//                Log.i("CompassApp", "Payload: " + payload);
+//                bridge.getWebView().post(() -> bridge.getWebView().evaluateJavascript("oauthRedirect(" + payload + ");", null));
+//            } catch (JSONException e) {
+//                Log.i("CompassApp", "Failed to encode JSON payload", e);
+//            }
+//        } else {
+//            Log.i("CompassApp", "No relevant data");
+//        }
     }
 
     @Override
@@ -99,5 +102,28 @@ public class MainActivity extends BridgeActivity {
         askNotificationPermission();
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN && requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+            PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
+            if (pluginHandle == null) {
+                Log.i("CompassApp", "SocialLogin login handle is null");
+                return;
+            }
+            Plugin plugin = pluginHandle.getInstance();
+            if (!(plugin instanceof SocialLoginPlugin)) {
+                Log.i("CompassApp", "SocialLogin plugin instance is not SocialLoginPlugin");
+                return;
+            }
+            Log.i("CompassApp", "handleGoogleLoginIntent");
+            ((SocialLoginPlugin) plugin).handleGoogleLoginIntent(requestCode, data);
+        }
+    }
+
+    // This function will never be called, leave it empty
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {}
 }
 

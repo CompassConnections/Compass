4cf783f07fb13ecb97e4e4903b88884bb60626a3
MartinBraquet <martin.braquet@gmail.com>
2025-11-03 16:19:10 +0100

    Add proxy

---
+++ web/pages/api/proxy-data.ts
@@ -0,0 +1,14 @@
+export default async function handler(req: any, res: any) {
+  const { path } = req.query; // e.g. "v4FoTtuyX_XwjC3aS2_Y6/Martin.json"
+  const upstreamUrl = `https://www.compassmeet.com/_next/data/${path}`;
+
+  const upstream = await fetch(upstreamUrl);
+  const body = await upstream.text();
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  res.status(upstream.status).send(body);
+}

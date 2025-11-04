ee2b8a60a243b2448dd0a522a8e5dd6325a0aef6
MartinBraquet <martin.braquet@gmail.com>
2025-11-04 13:03:43 +0100

    Increase notif lines

--- web/components/notification-items.tsx
+++ web/components/notification-items.tsx
@@ -54,7 +54,7 @@ export function BaseNotification(props: {
         <AvatarNotificationIcon notification={notification} />
       }
       subtitle={
-        <div className="line-clamp-2">
+        <div className="line-clamp-4">
           <Linkify text={notification.sourceText} />
         </div>
       }

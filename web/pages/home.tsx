60989faa03a80c408f5cb50d0b0e106b7b88cecc
MartinBraquet <martin.braquet@gmail.com>
2025-11-10 18:45:07 +0100

    Add TEST_DOWNTIME

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -2,8 +2,12 @@ import {PageBase} from 'web/components/page-base'
 import {Col} from 'web/components/layout/col'
 import {LoggedOutHome} from "web/components/home/home";
 
+export const TEST_DOWNTIME = true
 
-export default function ProfilesPage() {
+export default function Home() {
+  if (TEST_DOWNTIME) {
+    throw new Error('500 - This is a test error')
+  }
   return (
     <PageBase trackPageView={'home'}>
       <Col className="items-center">

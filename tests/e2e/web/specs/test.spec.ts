d0c0bd1a5efea5d7b61c5f708f77db9c62054716
n3c777 <n3c777@gmail.com>
2025-11-12 00:59:13 -0800

    Remove test file

--- tests/e2e/web/specs/test.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('should be logged in and see the dashboard', async ({ page }) => {
-  await page.goto('/Organization');
-  await expect(page.getByRole('link', {name: 'Organization'} )).toBeVisible(); 
-});

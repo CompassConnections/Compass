d790fae74a2a62f7f463aedaf57a1ff31bb656f4
MartinBraquet <martin.braquet@gmail.com>
2025-11-13 13:11:53 +0100

    Add e2e script for local development

---
+++ scripts/e2e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"/..
+
+npx playwright install chromium
+
+export NEXT_PUBLIC_API_URL=localhost:8088
+export NEXT_PUBLIC_FIREBASE_ENV=DEV
+
+yarn --cwd=web serve &
+npx wait-on http://localhost:3000
+npx playwright test tests/e2e
+SERVER_PID=$(fuser -k 3000/tcp)
+echo $SERVER_PID
+kill $SERVER_PID
+

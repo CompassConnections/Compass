0d48c541a08cdffcffa2994d4c5fdb22d74ad189
MartinBraquet <martin.braquet@gmail.com>
2025-11-15 00:27:30 +0100

    Add tests runs to all packages

---
+++ web/jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+
+    rootDir: '.',
+    testMatch: [
+        "<rootDir>/tests/**/*.test.ts",
+        "<rootDir>/tests/**/*.spec.ts"
+    ],
+
+    moduleNameMapper: {
+        "^api/(.*)$": "<rootDir>/src/$1",
+        "^shared/(.*)$": "<rootDir>/../shared/src/$1",
+        "^common/(.*)$": "<rootDir>/../../common/src/$1",
+        "^email/(.*)$": "<rootDir>/../email/emails/$1"
+    },
+
+    moduleFileExtensions: ["ts", "js", "json"],
+    clearMocks: true,
+
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.json'
+        }
+    }
+};

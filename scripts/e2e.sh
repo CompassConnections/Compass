f7fb0c6c82bc65f565c3f211fada678f85831c00
Nicholas Chamberlain <97712961+n3c777@users.noreply.github.com>
2025-11-18 14:17:00 -0800

    Test/e2e login auth (#19)
    * Add basic test for login and auth state
    
    * Remove test file
    
    * Change login setup
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Braquet <martin.braquet@gmail.com>
    
    * Change signin structure to use UI
    
    * Fix URL loading
    
    * Spin up backend server as well for E2E
    
    ---------
    
    Co-authored-by: Martin Braquet <martin.braquet@gmail.com>

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -10,9 +10,13 @@ export NEXT_PUBLIC_API_URL=localhost:8088
 export NEXT_PUBLIC_FIREBASE_ENV=DEV
 
 npx nyc --reporter=lcov yarn --cwd=web serve &
+npx nyc --reporter=lcov yarn --cwd=backend/api dev &
 npx wait-on http://localhost:3000
 npx playwright test tests/e2e
 SERVER_PID=$(fuser -k 3000/tcp)
 echo $SERVER_PID
 kill $SERVER_PID
 
+SERVER_PID=$(fuser -k 8088/tcp)
+echo $SERVER_PID
+kill $SERVER_PID

a0e48aa4c110784996ca0b08c7b705e871dfdc7e
Okechi Jones-Williams <okechi.williams@gmail.com>
2025-11-21 19:59:29 +0000

    .

--- backend/api/tests/unit/get-supabase-token.unit.test.ts
+++ backend/api/tests/unit/get-supabase-token.unit.test.ts
@@ -6,41 +6,41 @@ import * as constants from "common/envs/constants";
 import { AuthedUser } from "api/helpers/endpoint";
 
 describe.skip('getSupabaseToken', () => {
-    const originalSupabaseJwtSecret = process.env.SUPABASE_JWT_SECRET
-    const originalInstanceId = constants.ENV_CONFIG.supabaseInstanceId
-    const originalProjectId = constants.ENV_CONFIG.firebaseConfig.projectId
+    // const originalSupabaseJwtSecret = process.env.SUPABASE_JWT_SECRET
+    // const originalInstanceId = constants.ENV_CONFIG.supabaseInstanceId
+    // const originalProjectId = constants.ENV_CONFIG.firebaseConfig.projectId
 
-    describe('should', () => {
-        beforeEach(() => {
-            jest.resetAllMocks();
+    // describe('should', () => {
+    //     beforeEach(() => {
+    //         jest.resetAllMocks();
 
-            process.env.SUPABASE_JWT_SECRET = 'test-jwt-secret-123';
-            constants.ENV_CONFIG.supabaseInstanceId = 'test-instance-id';
-            constants.ENV_CONFIG.firebaseConfig.projectId = 'test-project-id';
+    //         process.env.SUPABASE_JWT_SECRET = 'test-jwt-secret-123';
+    //         constants.ENV_CONFIG.supabaseInstanceId = 'test-instance-id';
+    //         constants.ENV_CONFIG.firebaseConfig.projectId = 'test-project-id';
 
-            (jsonWebtokenModules.sign as jest.Mock).mockReturnValue('fake-jwt-token-abc123');
-        });
+    //         (jsonWebtokenModules.sign as jest.Mock).mockReturnValue('fake-jwt-token-abc123');
+    //     });
 
-        afterEach(() => {
-            if (originalSupabaseJwtSecret === undefined) {
-                delete process.env.SUPABASE_JWT_SECRET;
-            } else {
-                process.env.SUPABASE_JWT_SECRET = originalSupabaseJwtSecret;
-            }
-            constants.ENV_CONFIG.supabaseInstanceId = originalInstanceId;
-            constants.ENV_CONFIG.firebaseConfig.projectId = originalProjectId;
+    //     afterEach(() => {
+    //         if (originalSupabaseJwtSecret === undefined) {
+    //             delete process.env.SUPABASE_JWT_SECRET;
+    //         } else {
+    //             process.env.SUPABASE_JWT_SECRET = originalSupabaseJwtSecret;
+    //         }
+    //         constants.ENV_CONFIG.supabaseInstanceId = originalInstanceId;
+    //         constants.ENV_CONFIG.firebaseConfig.projectId = originalProjectId;
 
-            jest.restoreAllMocks();
-        });
+    //         jest.restoreAllMocks();
+    //     });
 
-        it('successfully generate a JTW token with correct parameters', async () => {
-            const mockParams = {} as any;
-            const mockAuth = {uid: '321'} as AuthedUser;
-            const result = await getSupabaseToken(mockParams, mockAuth, mockParams)
+    //     it('successfully generate a JTW token with correct parameters', async () => {
+    //         const mockParams = {} as any;
+    //         const mockAuth = {uid: '321'} as AuthedUser;
+    //         const result = await getSupabaseToken(mockParams, mockAuth, mockParams)
 
-            expect(result).toEqual({
-                jwt: 'fake-jwt-token-abc123'
-            })
-        })
-    });
+    //         expect(result).toEqual({
+    //             jwt: 'fake-jwt-token-abc123'
+    //         })
+    //     })
+    // });
 });

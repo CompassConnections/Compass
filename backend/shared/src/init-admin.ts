e04656c4b3d6dcf7a723853849125012fc33bd95
n3c777 <n3c777@gmail.com>
2025-12-02 14:09:10 -0800

    add firebase emulator, add registration script, add signup spec

--- backend/shared/src/init-admin.ts
+++ backend/shared/src/init-admin.ts
@@ -1,15 +1,25 @@
 import * as admin from 'firebase-admin'
+import { getServiceAccountCredentials } from "shared/firebase-utils";
+import { IS_LOCAL } from "common/hosting/constants";
 
-
-import {getServiceAccountCredentials} from "shared/firebase-utils";
-import {IS_LOCAL} from "common/hosting/constants";
-
-// Locally initialize Firebase Admin.
 export const initAdmin = () => {
+
+  if (IS_LOCAL && process.env.USE_FIREBASE_EMULATOR === "true") {
+    console.log("Using Firebase Emulator Suite.")
+
+    process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099"
+    process.env.FIREBASE_STORAGE_EMULATOR_HOST = "127.0.0.1:9199" // Default Storage port
+
+    return admin.initializeApp({
+      projectId: "compass-57c3c",
+      storageBucket: "compass-130ba-public",
+    })
+  }
+
   if (IS_LOCAL) {
     try {
       const serviceAccount = getServiceAccountCredentials()
-      // console.debug(serviceAccount)
+      
       if (!serviceAccount.project_id) {
         console.debug(`GOOGLE_APPLICATION_CREDENTIALS not set, skipping admin firebase init.`)
         return
@@ -24,7 +34,7 @@ export const initAdmin = () => {
       console.error(err)
     }
   }
-
+  
   console.debug(`Initializing connection to default Firebase...`)
   return admin.initializeApp()
 }

ef7665c7dae2b9afc5bc2c3b021d2086a8d709b4
Nicholas Chamberlain <97712961+n3c777@users.noreply.github.com>
2025-12-06 14:43:46 -0800

    Add firebase emulator, Add registration script, Add signup spec (#22)
    * add firebase emulator, add registration script, add signup spec
    
    * Upgrade firebase emulator and make it pass the E2E tests
    
    ---------
    
    Co-authored-by: MartinBraquet <martin.braquet@gmail.com>

---
+++ tests/e2e/web/fixtures/deleteUserFixture.ts
@@ -0,0 +1,42 @@
+import { test as base } from '@playwright/test';
+import axios from 'axios';
+import { config } from '../SPEC_CONFIG';
+
+const baseUrl = 'http://localhost:9099/identitytoolkit.googleapis.com/v1';
+
+async function deleteUser(email: string, password: string) {
+  try {
+    const login = await axios.post(
+      `${baseUrl}/accounts:signInWithPassword?key=fake-api-key`,
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    );
+
+    await axios.post(
+      `${baseUrl}/accounts:delete?key=fake-api-key`,
+      { idToken: login.data.idToken }
+    );
+  } catch (err: any) {
+    console.log(err);
+  }
+}
+
+type CleanupFixtures = {
+  cleanupUsers: void;
+};
+
+export const test = base.extend<CleanupFixtures>({
+  cleanupUsers: [
+    async ({}, use) => {
+      // Run all tests first
+      await use();
+
+      //then delete users
+      await deleteUser(config.USERS.SPEC.EMAIL, config.USERS.SPEC.PASSWORD);
+    },
+    { auto: true },
+  ],
+});

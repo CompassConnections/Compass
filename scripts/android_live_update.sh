c0b8df4ef9605a420d165835a6e27f747ef471fc
MartinBraquet <martin.braquet@gmail.com>
2025-12-14 23:39:48 +0200

    Fix

--- scripts/android_live_update.sh
+++ scripts/android_live_update.sh
@@ -9,7 +9,4 @@ yarn build-web-view
 npx @capawesome/cli apps:bundles:create \
   --app-id 969bc540-8077-492f-8403-b554bee5de50 \
   --channel default \
-  --commit-message $(git log -1 --pretty=format:"%s") \
-  --commit-ref 'd699ceae3890f70a7bc66720bc980398f799a6e1' \
-  --commit-sha 'd699ceae3890f70a7bc66720bc980398f799a6e1' \
   --path web/out

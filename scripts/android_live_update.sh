dca08a6d81778cad13aaa3378a43c22ea763986a
MartinBraquet <martin.braquet@gmail.com>
2025-12-15 10:49:21 +0200

    Clean

--- scripts/android_live_update.sh
+++ scripts/android_live_update.sh
@@ -9,7 +9,7 @@ yarn build-web-view
 echo npx @capawesome/cli apps:bundles:create \
        --app-id 969bc540-8077-492f-8403-b554bee5de50 \
        --channel default \
-       --commitMessage $(git log -1 --pretty=format:"%s") \
+       --commitMessage "$(git log -1 --pretty=format:"%s")" \
        --commitRef $commitRef \
        --commitSha $commitSha \
        --path web/out

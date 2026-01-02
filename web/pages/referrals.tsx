33c2121c8d1f89679f687e734d2ae8079a6a26dd
MartinBraquet <martin.braquet@gmail.com>
2026-01-02 14:22:46 +0200

    Translate /referrals

--- web/pages/referrals.tsx
+++ web/pages/referrals.tsx
@@ -6,25 +6,27 @@ import { Col } from 'web/components/layout/col'
 import { QRCode } from 'web/components/widgets/qr-code'
 import { Title } from 'web/components/widgets/title'
 import { useUser } from 'web/hooks/use-user'
+import {useT} from "web/lib/locale";
 
 export default function ReferralsPage() {
   const user = useUser()
+  const t = useT()
 
   const url = user
     ? `https://${ENV_CONFIG.domain}/?referrer=${user.username}`
     : `https://${ENV_CONFIG.domain}/`
 
+  const title = t('referrals.title', `Invite someone to join Compass!`)
+
   return (
     <PageBase trackPageView={'referrals'} className="items-center">
       <SEO
         title="Compass"
-        description={`Invite someone to join Compass!`}
+        description={title}
       />
 
       <Col className="bg-canvas-0 rounded-lg p-4 sm:p-8">
-        <Title>Invite someone to join Compass!</Title>
-
-        <div className="mb-4">Invite someone to join</div>
+        <Title>{title}</Title>
 
         <CopyLinkRow url={url} eventTrackingName="copyreferral" />
 

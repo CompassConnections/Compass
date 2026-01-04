cf104088ff051165ac2130a85759891ca8faa8bb
Okechi Jones-Williams <okechi.williams@gmail.com>
2026-01-04 22:30:14 +0000

    Added more unit tests

---
+++ backend/api/tests/unit/get-me.unit.test.ts
@@ -0,0 +1,29 @@
+jest.mock('api/get-user');
+
+import { getMe } from "api/get-me";
+import { getUser } from "api/get-user";
+import { AuthedUser } from "api/helpers/endpoint";
+
+describe('getMe', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('when given valid input', () => {
+        it('should get the user', async () => {
+            const mockProps = {};
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+
+            (getUser as jest.Mock).mockResolvedValue(null);
+
+            await getMe(mockProps, mockAuth, mockReq);
+
+            expect(getUser).toBeCalledTimes(1);
+            expect(getUser).toBeCalledWith({id: mockAuth.uid});
+        });
+    });
+});
---
+++ backend/api/tests/unit/get-messages-count.unit.test.ts
@@ -0,0 +1,40 @@
+jest.mock('shared/supabase/init');
+
+import { getMessagesCount } from "api/get-messages-count";
+import { AuthedUser } from "api/helpers/endpoint";
+import * as supabaseInit from "shared/supabase/init";
+
+describe('getMessagesCount', () => {
+    let mockPg = {} as any;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        mockPg = {
+            one: jest.fn()
+        };
+        (supabaseInit.createSupabaseDirectClient as jest.Mock)
+            .mockReturnValue(mockPg);
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('when given valid input', () => {
+        it('should get message count', async () => {
+            const mockProps = {} as any;
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            const mockResults = { count: "10"};
+
+            (mockPg.one as jest.Mock).mockResolvedValue(mockResults);
+
+            const result: any = await getMessagesCount(mockProps, mockAuth, mockReq);
+
+            expect(result.count).toBe(Number(mockResults.count));
+            expect(mockPg.one).toBeCalledTimes(1);
+            expect(mockPg.one).toBeCalledWith(
+                expect.stringContaining('SELECT COUNT(*) AS count'),
+                expect.any(Object)
+            );
+        });
+    });
+});

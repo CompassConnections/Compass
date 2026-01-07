e4c877e928668a81775d0ac86c625016ea8b6893
Okechi Jones-Williams <okechi.williams@gmail.com>
2026-01-07 13:48:17 +0000

    Added unit tests

---
+++ backend/api/tests/unit/get-profile-answers.unit.test.ts
@@ -0,0 +1,53 @@
+jest.mock('shared/supabase/init');
+
+import { getProfileAnswers } from "api/get-profile-answers";
+import { AuthedUser } from "api/helpers/endpoint";
+import * as supabaseInit from "shared/supabase/init";
+
+describe('getProfileAnswers', () => {
+    let mockPg = {} as any;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        mockPg = {
+            manyOrNone: jest.fn()
+        };
+
+        (supabaseInit.createSupabaseDirectClient as jest.Mock)
+            .mockReturnValue(mockPg);
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('when given valid input', () => {
+        it('should get the answers for the userId', async () => {
+            const mockProps = { userId: "mockUserId" };
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            const mockAnswers = [
+                {
+                    created_time: "mockCreatedTime",
+                    creator_id: "mockCreatorId",
+                    explanation: "mockExplanation",
+                    id: 123,
+                    importance: 10,
+                    multiple_choice: 1234,
+                    pref_choices: [1, 2, 3],
+                    question_id: 12345
+                }
+            ];
+
+            (mockPg.manyOrNone as jest.Mock).mockResolvedValue(mockAnswers);
+
+            const result: any = await getProfileAnswers(mockProps, mockAuth, mockReq);
+
+            expect(result.status).toBe('success');
+            expect(result.answers).toBe(mockAnswers);
+            expect(mockPg.manyOrNone).toBeCalledTimes(1);
+            expect(mockPg.manyOrNone).toBeCalledWith(
+                expect.stringContaining('select * from compatibility_answers'),
+                [mockProps.userId]
+            );
+        });    
+    });
+});
---
+++ backend/api/tests/unit/has-free-like.unit.test.ts
@@ -0,0 +1,57 @@
+jest.mock('shared/supabase/init');
+
+import * as freeLikeModule from "api/has-free-like";
+import { AuthedUser } from "api/helpers/endpoint";
+import * as supabaseInit from "shared/supabase/init";
+
+describe('hasFreeLike', () => {
+    let mockPg = {} as any;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        mockPg = {
+            oneOrNone: jest.fn()
+        };
+
+        (supabaseInit.createSupabaseDirectClient as jest.Mock)
+            .mockReturnValue(mockPg);
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('when given valid input', () => {
+        it('should return if the user has a free like', async () => {
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            const mockProps = {} as any;
+
+            jest.spyOn( freeLikeModule, 'getHasFreeLike');
+            (mockPg.oneOrNone as jest.Mock).mockResolvedValue(false);
+
+            const result: any = await freeLikeModule.hasFreeLike(mockProps, mockAuth, mockReq);
+
+            expect(result.status).toBe('success');
+            expect(result.hasFreeLike).toBeTruthy();
+            expect(freeLikeModule.getHasFreeLike).toBeCalledTimes(1);
+            expect(freeLikeModule.getHasFreeLike).toBeCalledWith(mockAuth.uid);
+            expect(mockPg.oneOrNone).toBeCalledTimes(1);
+            expect(mockPg.oneOrNone).toBeCalledWith(
+                expect.stringContaining('from profile_likes'),
+                [mockAuth.uid]
+            );
+        });    
+
+        it('should return if the user does not have a free like', async () => {
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            const mockProps = {} as any;
+
+            jest.spyOn( freeLikeModule, 'getHasFreeLike');
+            (mockPg.oneOrNone as jest.Mock).mockResolvedValue(true);
+
+            const result: any = await freeLikeModule.hasFreeLike(mockProps, mockAuth, mockReq);
+
+            expect(result.hasFreeLike).toBeFalsy();
+        });    
+    });
+});

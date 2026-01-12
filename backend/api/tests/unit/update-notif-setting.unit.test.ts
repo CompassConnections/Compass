c8804b2d46fdf8ca95e557e5b0578157b29eb545
Okechi Jones-Williams <okechi.williams@gmail.com>
2026-01-12 01:07:29 +0000

    Added api unit tests

---
+++ backend/api/tests/unit/update-notif-setting.unit.test.ts
@@ -0,0 +1,71 @@
+jest.mock('shared/supabase/init');
+jest.mock('shared/supabase/users');
+jest.mock('shared/websockets/helpers');
+
+import { AuthedUser } from "api/helpers/endpoint";
+import { updateNotifSettings } from "api/update-notif-setting";
+import * as supabaseInit from "shared/supabase/init";
+import * as supabaseUsers from "shared/supabase/users";
+import * as websocketHelpers from "shared/websockets/helpers";
+
+describe('updateNotifSettings', () => {
+    let mockPg = {} as any;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        mockPg = {
+            none: jest.fn()
+        };
+
+        (supabaseInit.createSupabaseDirectClient as jest.Mock)
+            .mockReturnValue(mockPg);
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('when given valid input', () => {
+        it('should update notification settings', async () => {
+            const mockProps = {
+                type: "new_match" as const,
+                medium: "email" as const,
+                enabled: false
+            };
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+
+            (mockPg.none as jest.Mock).mockResolvedValue(null);
+            (websocketHelpers.broadcastUpdatedPrivateUser as jest.Mock).mockReturnValue(null);
+
+            await updateNotifSettings(mockProps, mockAuth, mockReq);
+
+            expect(mockPg.none).toBeCalledTimes(1);
+            expect(mockPg.none).toBeCalledWith(
+                expect.stringContaining('update private_users'),
+                [mockProps.type, mockProps.medium, mockAuth.uid]
+            );
+            expect(websocketHelpers.broadcastUpdatedPrivateUser).toBeCalledTimes(1);
+            expect(websocketHelpers.broadcastUpdatedPrivateUser).toBeCalledWith(mockAuth.uid);
+        });    
+
+        it('should turn off notifications', async () => {
+            const mockProps = {
+                type: "opt_out_all" as const,
+                medium: "mobile" as const,
+                enabled: true
+            };
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+
+            (supabaseUsers.updatePrivateUser as jest.Mock).mockResolvedValue(null);
+
+            await updateNotifSettings(mockProps, mockAuth, mockReq);
+
+            expect(supabaseUsers.updatePrivateUser).toBeCalledTimes(1);
+            expect(supabaseUsers.updatePrivateUser).toBeCalledWith(
+                expect.any(Object),
+                mockAuth.uid,
+                {interestedInPushNotifications: !mockProps.enabled}
+            );
+        });    
+    });
+});
---
+++ backend/api/tests/unit/update-private-user-message-channel.unit.test.ts
@@ -0,0 +1,91 @@
+jest.mock('shared/supabase/init');
+jest.mock('shared/utils');
+jest.mock('common/supabase/utils');
+
+import {updatePrivateUserMessageChannel} from "api/update-private-user-message-channel";
+import * as supabaseInit from "shared/supabase/init";
+import * as sharedUtils from "shared/utils";
+import * as supabaseUtils from "common/supabase/utils";
+import { AuthedUser } from "api/helpers/endpoint";
+
+describe('updatePrivateUserMessageChannel', () => {
+    let mockPg = {} as any;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        mockPg = {
+            oneOrNone: jest.fn(),
+            none: jest.fn()
+        };
+
+        (supabaseInit.createSupabaseDirectClient as jest.Mock)
+            .mockReturnValue(mockPg);
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    describe('when given valid input', () => {
+        it('should return success after updating the users private message channel', async () => {
+            const mockBody = {
+                channelId: 123,
+                notifyAfterTime: 10
+            };
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            
+            (sharedUtils.getUser as jest.Mock).mockResolvedValue(true);
+            (mockPg.oneOrNone as jest.Mock).mockResolvedValue(true);
+            (supabaseUtils.millisToTs as jest.Mock).mockReturnValue('mockMillisToTs');
+
+            const results = await updatePrivateUserMessageChannel(mockBody, mockAuth, mockReq);
+
+            expect(results.status).toBe('success');
+            expect(results.channelId).toBe(mockBody.channelId);
+            expect(sharedUtils.getUser).toBeCalledTimes(1);
+            expect(sharedUtils.getUser).toBeCalledWith(mockAuth.uid);
+            expect(mockPg.oneOrNone).toBeCalledTimes(1);
+            expect(mockPg.oneOrNone).toBeCalledWith(
+                expect.stringContaining('select status from private_user_message_channel_members'),
+                [mockBody.channelId, mockAuth.uid]
+            );
+            expect(mockPg.none).toBeCalledTimes(1);
+            expect(mockPg.none).toBeCalledWith(
+                expect.stringContaining('update private_user_message_channel_members'),
+                [mockBody.channelId, mockAuth.uid, 'mockMillisToTs']
+            );
+        });
+    });
+    describe('when an error occurs', () => {
+        it('should throw if the user account does not exist', async () => {
+            const mockBody = {
+                channelId: 123,
+                notifyAfterTime: 10
+            };
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            
+            (sharedUtils.getUser as jest.Mock).mockResolvedValue(false);
+
+            expect(updatePrivateUserMessageChannel(mockBody, mockAuth, mockReq))
+                .rejects
+                .toThrow('Your account was not found');
+        });
+
+        it('should throw if the user is not authorized in the channel', async () => {
+            const mockBody = {
+                channelId: 123,
+                notifyAfterTime: 10
+            };
+            const mockAuth = { uid: '321' } as AuthedUser;
+            const mockReq = {} as any;
+            
+            (sharedUtils.getUser as jest.Mock).mockResolvedValue(true);
+            (mockPg.oneOrNone as jest.Mock).mockResolvedValue(false);
+
+            expect(updatePrivateUserMessageChannel(mockBody, mockAuth, mockReq))
+                .rejects
+                .toThrow('You are not authorized to this channel');
+
+        });
+    });
+});

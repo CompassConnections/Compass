3a4ad9405518cebed38a3c901ef2b9690cdb05da
MartinBraquet <martin.braquet@gmail.com>
2026-03-01 00:01:59 +0100

    Back to good one

--- tests/e2e/utils/databaseUtils.ts
+++ tests/e2e/utils/databaseUtils.ts
@@ -2,45 +2,46 @@ import {createSupabaseDirectClient} from 'shared/supabase/init'
 
 export async function deleteFromDb(user_id: string) {
   const db = createSupabaseDirectClient()
-  try {
-    const deleteEntryById = `DELETE FROM users WHERE id = $1 RETURNING *`
-    const result = await db.query(deleteEntryById, [user_id])
+  const deleteEntryById = `DELETE FROM users WHERE id = $1 RETURNING *`
+  const result = await db.query(deleteEntryById, [user_id])
 
-    if (!result.length) {
-      throw new Error(`No user found with id: ${user_id}`)
-    }
-
-    console.log('Deleted data: ', {
-      id: result[0].id,
-      name: result[0].name,
-      username: result[0].username,
-    })
-  } catch (error) {
-    throw error
+  if (!result.length) {
+    throw new Error(`No user found with id: ${user_id}`)
   }
+
+  console.log('Deleted data: ', {
+    id: result[0].id,
+    name: result[0].name,
+    username: result[0].username,
+  })
 }
 
 export async function userInformationFromDb(account: any) {
   const db = createSupabaseDirectClient()
-  try {
-    const queryUserById = `
+  const queryUserById = `
                 SELECT p.*
                 FROM users AS p
-                WHERE name = $1
+                WHERE username = $1
             `
-    const userResults = await db.query(queryUserById, [account.display_name])
-    const queryProfileById = `
+  const userResults = await db.query(queryUserById, [account.username])
+
+  if (userResults.length === 0) {
+    throw new Error(`No user found with username: ${account.username}`)
+  }
+
+  const queryProfileById = `
                 SELECT p.*
                 FROM profiles AS p
                 WHERE user_id = $1
             `
-    const profileResults = await db.query(queryProfileById, [userResults[0].id])
+  const profileResults = await db.query(queryProfileById, [userResults[0].id])
 
-    return {
-      user: userResults[0],
-      profile: profileResults[0],
-    }
-  } catch (error) {
-    throw error
+  if (profileResults.length === 0) {
+    throw new Error(`No profile found for user: ${userResults[0].id}`)
+  }
+
+  return {
+    user: userResults[0],
+    profile: profileResults[0],
   }
 }

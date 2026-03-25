04c18aee7c96ab1f9668eb6f71c510a132798914
Okechi Jones-Williams <okechi.williams@gmail.com>
2026-03-25 01:09:24 +0000

    Linting and Prettier

--- tests/e2e/web/specs/postSignIn.spec.ts
+++ tests/e2e/web/specs/postSignIn.spec.ts
@@ -1,19 +1,15 @@
 import {test, expect} from '../fixtures/signInFixture'
-import * as fs from "fs";
+import * as fs from 'fs'
 
 test.describe('when given valid input', () => {
-  test('should be logged in and see settings page', async ({ 
-    authenticatedPage,
-    homePage,
-  }) => {
+  test('should be logged in and see settings page', async ({authenticatedPage, homePage}) => {
     await homePage.gotToHomePage()
     // await authenticatedPage.goto('/settings')
 
     // await expect(authenticatedPage.getByRole('heading', { name: 'Theme' })).toBeVisible()
   })
-});
+})
 
 test.describe('when an error occurs', () => {
-  test('placeholder', async () => {});
-});
-
+  test('placeholder', async () => {})
+})

a0e4794ff0e8b5989394e39c19384ae340911dac
Okechi Jones-Williams <okechi.williams@gmail.com>
2026-04-04 19:17:45 +0100

    Changed POM/Fixture structure to use an app class to instantiate the page objects

--- tests/e2e/web/specs/onboardingFlow.spec.ts
+++ tests/e2e/web/specs/onboardingFlow.spec.ts
@@ -251,65 +251,6 @@ test.describe('when given valid input', () => {
     await expect(dbInfo.user.username).toContain(fakerAccount.username)
   })
 
-  test('should successfully delete an account created via email and password', async ({
-    homePage,
-    onboardingPage,
-    signUpPage,
-    authPage,
-    profilePage,
-    settingsPage,
-    fakerAccount,
-  }) => {
-    await registerWithEmail(homePage, authPage, fakerAccount)
-    await skipOnboardingHeadToProfile(onboardingPage, signUpPage, profilePage, fakerAccount)
-
-    //Verify displayed information is correct
-    await profilePage.verifyDisplayName(fakerAccount.display_name)
-
-    //Verify database info
-    const dbInfo = await userInformationFromDb(fakerAccount)
-
-    await expect(dbInfo.user.name).toContain(fakerAccount.display_name)
-    await expect(dbInfo.user.username).toContain(fakerAccount.username)
-
-    await deleteProfileFromSettings(homePage, settingsPage)
-  })
-
-  test('should successfully delete an account created via google auth', async ({
-    homePage,
-    onboardingPage,
-    signUpPage,
-    authPage,
-    profilePage,
-    settingsPage,
-    googleAccountTwo,
-    headless,
-  }) => {
-    console.log(
-      `Starting "should successfully delete an account created via google auth" with ${googleAccountTwo.username}`,
-    )
-    test.skip(headless, 'Google popup auth test requires headed mode')
-    await homePage.goToRegisterPage()
-    await authPage.fillPasswordField('') //The test only passes when this is added...something is weird here
-    await authPage.signInToGoogleAccount(
-      googleAccountTwo.email,
-      googleAccountTwo.display_name,
-      googleAccountTwo.username,
-    )
-    await skipOnboardingHeadToProfile(onboardingPage, signUpPage, profilePage, googleAccountTwo)
-
-    //Verify displayed information is correct
-    await profilePage.verifyDisplayName(googleAccountTwo.display_name)
-
-    //Verify database info
-    const dbInfo = await userInformationFromDb(googleAccountTwo)
-
-    await expect(dbInfo.user.name).toContain(googleAccountTwo.display_name)
-    await expect(dbInfo.user.username).toContain(googleAccountTwo.username)
-
-    await deleteProfileFromSettings(homePage, settingsPage)
-  })
-
   test('should successfully enter optional information after completing flow', async ({
     app,
     fakerAccount,

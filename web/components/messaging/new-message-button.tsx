9163219eb2f58ce292a9797630b9b640d1220c6f
MartinBraquet <martin.braquet@gmail.com>
2026-05-09 10:58:55 +0200

    Update NewMessageButton: change color to 'primary' for improved emphasis

--- web/components/messaging/new-message-button.tsx
+++ web/components/messaging/new-message-button.tsx
@@ -20,11 +20,7 @@ export default function NewMessageButton() {
   const t = useT()
   return (
     <>
-      <Button
-        className="h-fit gap-1 bg-canvas-50"
-        color={'gray-outline'}
-        onClick={() => setOpen(true)}
-      >
+      <Button className="h-fit gap-1 bg-canvas-50" color={'primary'} onClick={() => setOpen(true)}>
         <PlusIcon className="h-5 w-5" aria-hidden="true" />
         {t('messages.new_message', 'New Message')}
       </Button>

eaa4aa9a48e7d68b4db27f157f8d7d6c7d5bf533
MartinBraquet <martin.braquet@gmail.com>
2026-05-11 14:17:10 +0200

    Refactor `profile-info.tsx`: simplify `clsx` usage by re-enabling conditional `ml-6` class for better readability

--- web/components/profile/profile-info.tsx
+++ web/components/profile/profile-info.tsx
@@ -138,10 +138,7 @@ export function ProfileInfo(props: {
             {user.name?.charAt(0).toUpperCase()}
           </div>
           <div
-            className={clsx(
-              'min-w-0 flex-1 animate-profile-appear',
-              // !profile.pinned_url && 'ml-6'
-            )}
+            className={clsx('min-w-0 flex-1 animate-profile-appear', !profile.pinned_url && 'ml-6')}
           >
             <ProfileHeader
               user={user}
